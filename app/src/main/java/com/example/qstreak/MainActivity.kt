d4ceea84aa7f64c4cd0e0a1a3bc8faf03a038949
Evan Barger <epbarger@gmail.com>
2020-04-07 17:15:10 -0400

    Attempt to hook up main recycler view with submissions adapter and observe data

--- app/src/main/java/com/example/qstreak/MainActivity.kt
+++ app/src/main/java/com/example/qstreak/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.qstreak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.qstreak.ui.SubmissionsAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.submissions_recycler_view)
+        val adapter = SubmissionsAdapter()
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        mainViewModel.submissions.observe(this, Observer { submissions ->
+            submissions?.let { adapter.setSubmissions(it) }
+        })
     }
 }
