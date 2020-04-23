4e88456c43f57ceeb34e2d6aee223efe99741536
Rebecca Blum <schmecs@gmail.com>
2020-04-23 15:37:25 -0400

    Remove unused imports

--- app/src/main/java/com/example/qstreak/network/QstreakApiSignupService.kt
+++ app/src/main/java/com/example/qstreak/network/QstreakApiSignupService.kt
@@ -1,17 +1,8 @@
 package com.example.qstreak.network
 
-import com.example.qstreak.BuildConfig
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Response
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
-import timber.log.Timber
-import java.nio.charset.Charset
-import java.nio.charset.StandardCharsets
 
 interface QstreakApiSignupService {
 
