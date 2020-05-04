f72e126d0ad6e87fedfa7b84db5fa9faef2825d3
Rebecca Blum <schmecs@gmail.com>
2020-05-04 12:34:52 -0400

    Add dependency on submission repository for dashboard

--- app/src/main/java/com/example/qstreak/viewmodels/DashboardViewModel.kt
+++ app/src/main/java/com/example/qstreak/viewmodels/DashboardViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.qstreak.viewmodels
 
+import android.text.format.DateUtils
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.qstreak.db.SubmissionRepository
 
-class DashboardViewModel : ViewModel() {
+class DashboardViewModel(private val submissionRepository: SubmissionRepository) : ViewModel() {
     val testString = MutableLiveData("Data from dashboard viewmodel")
 }
